6604b27bedb93e8dd810fed6dfea9994cf377d6a
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2024-04-09 19:04:11 +0700

    update v2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './app.vue'
+import app from './app.vue'
 import './assets/tailwind.css'
 import './app.css'
 import router from './routers/router'
-createApp(App).use(router).mount('#app')
+createApp(app).use(router).mount('#app')
